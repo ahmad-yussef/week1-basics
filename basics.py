1297a4fbb246c1dbb4c670f3c2a9c77c14e915e2
Ahmad Yousef <ahmad.yussefb@gmail.com>
2025-09-23 21:53:07 +0300

    Day 1: hello world, basics, and user inputs

---
+++ basics.py
@@ -0,0 +1,8 @@
+name = "Alice" 
+age = 25
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("is stu?", is_student)
+
---
+++ userinp.py
@@ -0,0 +1,4 @@
+name = input("What's your name? ")
+age = int(input("how old are you? "))
+
+print(f"hello {name}, next year youll be {age+1}")
