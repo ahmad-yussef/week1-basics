4ca6420e7365087962f38cda3ebbcab2219a4838
Ahmad Yousef <ahmad.yussefb@gmail.com>
2025-09-23 23:34:54 +0300

    Day 2: conditionals practice

---
+++ conditionals.py
@@ -0,0 +1,22 @@
+#conditionals.py - Day 2: if/ elif/ else examples
+
+#example 1: basic if/else
+age = int(input("How old are you? "))
+if age >= 18:
+    print("You are an adult.")
+elif age < 18 :
+    print("You are a minor.")
+
+
+
+#example 2: Grade to letter
+grade = int(input("Enter your grade (0-100)"))
+if grade >= 90:
+    print("A")
+elif grade >=80:
+    print("B")
+elif grade >= 70:
+    print("C")
+else:
+    print("F")
+
